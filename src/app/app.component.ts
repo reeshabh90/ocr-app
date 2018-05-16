c9242a3084fbb2ca193386dbbf4695bf51151222
reeshabh90 <reeshabh.007@gmail.com>
2018-05-16 18:17:33 +0530

    Image Post processing changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,9 +35,22 @@ export class AppComponent {
     return this.trigger.asObservable();
   }
 
-  imageCropped(image: string) {
-    this.croppedImage = image;
+  imageCropped(img: string) {
+    const self = this;
+    const image = new Image();
+    image.onload = function () {
+      const canvas: any = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(image, 0, 0);
+      const imgD = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      const pixels = imgD.data;
+      self.convertToBW(image, canvas);
+      self.croppedImage = canvas.toDataURL();
+    };
+    image.id = 'tempImg';
+    image.src = img;
   }
+
   imageLoaded() {
     this.cropperReady = true;
   }
@@ -48,26 +61,31 @@ export class AppComponent {
   // Function to perform OCR
   performOCR() {
     const img: any = document.getElementById('img');
-    const canvas: any = document.createElement('canvas');
-
-    this.convertToBW(img, canvas);
-    Tesseract.recognize(img.src)
+    Tesseract.recognize(img)
       .then(function (result) {
-        // console.log('ocr result', result);
+        console.log('ocr result', result);
         document.getElementById('ocr_results')
           .innerText = result.text;
-        img.src = canvas.toDataURL();
       });
   }
 
-  // Function to convert Image to Binary or B&W
+  // Function to convert Image to Binary B&W
   convertToBW(img, canvas) {
-
+    // console.log('Image for processing', img);
     const ctx = canvas.getContext('2d');
     ctx.drawImage(img, 0, 0);
     const imgD = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const d = imgD.data;
     const pixels = imgD.data;
+
+    // 1st Step, convert to GreyScale
+    for (let i = 0; i < pixels.length; i += 4) {
+      const avg = (pixels[i] + pixels[i + 1] + pixels[i + 2]) / 3;
+      pixels[i] = avg; // red
+      pixels[i + 1] = avg; // green
+      pixels[i + 2] = avg; // blue
+    }
+
+    // 2nd Step, convert to Binary Black & White
     for (let i = 0, n = pixels.length; i < n; i += 4) {
       const gray = 0.2126 * pixels[i] + 0.7152 * pixels[i + 1] + 0.0722 * pixels[i + 2];
       if (gray > 128) {
@@ -84,5 +102,35 @@ export class AppComponent {
     console.log('Canvas base 64', canvas.toDataURL());
   }
 
+  normalize(pixels) {
+    const sum = pixels.reduce((a, b) => a + b);
+    for (let i = 0; i < pixels.length; ++i) {
+      pixels[i] /= sum;
+    }
 
+  }
+
+  equalizeHistogram(pixels) {
+    const srcLength = pixels.length;
+
+    // Compute histogram and histogram sum:
+    const hist = new Float32Array(256);
+    let sum = 0;
+    for (let i = 0; i < srcLength; ++i) {
+      ++hist[~~pixels[i]];
+      ++sum;
+    }
+
+    // Compute integral histogram:
+    let prev = hist[0];
+    for (let i = 1; i < 256; ++i) {
+      prev = hist[i] += prev;
+    }
+
+    // Equalize image:
+    const norm = 255 / sum;
+    for (let i = 0; i < srcLength; ++i) {
+      pixels[i] = hist[~~pixels[i]] * norm;
+    }
+  }
 }
